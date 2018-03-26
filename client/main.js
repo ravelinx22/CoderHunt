a381f449d6f3f2796f330c05432634ab2b02a7d4
William Ravelo <drummerwilliam@gmail.com>
2018-03-26 00:44:11 -0500

    added navbar toggle to home navbar

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { renderRoutes } from "../imports/startup/client/routes.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import '@trendmicro/react-toggle-switch/dist/react-toggle-switch.css';
 
 Meteor.startup(() => {
   render(renderRoutes(), document.getElementById('render-target'));
--- imports/ui/components/navbars/HomeNavbar.jsx
+++ imports/ui/components/navbars/HomeNavbar.jsx
@@ -2,24 +2,37 @@ import React, { Component } from "react";
 import "../../css/HomeNavbar.css";
 import { Link } from "react-router-dom"
 import { Container, Row, Col, Button } from "reactstrap";
+import ToggleSwitch from '@trendmicro/react-toggle-switch';
 
 export default class HomeNavbar extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			checked: false,
+			mode: "Programmer",
+		};
 	}
 
 	componentDidMount() {
+	}
+
+	toggleSwitch(event) {
+		this.setState({ 
+			checked: !this.state.checked,
+			mode: "Project"
+		});
 	}
 
 	render() {  
 		return(
 			<Container className="home_navbar d-flex align-items-center">
 				<Row className="home_navbar_row">
-					<div className="mr-auto">
-						Go Back
+					<div className="home_navbar_title mr-auto">
+						Start Swiping
 					</div>
 					<div className="ml-auto">
-						aslkjsaf
+						<span className="switch_label">{this.state.mode} mode</span>
+						<ToggleSwitch className="home_navbar_switch" checked={this.state.checked} onChange={this.toggleSwitch.bind(this)}/>
 					</div>
 				</Row>
 			</Container>
