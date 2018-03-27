6001e151aee0c38391a95d83c1b3db24671fab2c
William Ravelo <drummerwilliam@gmail.com>
2018-03-27 14:55:41 -0500

    added create new project form

--- imports/ui/pages/NewProjectPage.jsx
+++ imports/ui/pages/NewProjectPage.jsx
@@ -2,17 +2,54 @@ import React, { Component } from "react";
 import NewProjectNavbar from "../components/navbars/NewProjectNavbar.jsx";
 import "../css/NewProjectPage.css";
 import { Container, Row, Col, Button } from "reactstrap";
+import { WithContext as ReactTags } from 'react-tag-input';
 
 export default class NewProjectPage extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			tags: [{ text: "Thailand" }, { text: "India" }],
+			suggestions: ['USA', 'Germany', 'Austria', 'Costa Rica', 'Sri Lanka', 'Thailand']
+		};
+		this.handleDelete = this.handleDelete.bind(this);
+		this.handleAddition = this.handleAddition.bind(this);
+		this.handleDrag = this.handleDrag.bind(this);
 	}
 
 	componentDidMount() {
 		console.log(this.props);
 	}
 
+	handleDelete(i) {
+		const { tags } = this.state;
+		this.setState({
+			tags: tags.filter((tag, index) => index !== i),
+		});
+	}
+
+	handleAddition(tag) {
+		let tags = this.state.tags;
+		tags.push({
+			text: tag
+		});
+		this.setState({tags: tags});
+	}
+
+	handleDrag(tag, currPos, newPos) {
+		const tags = [...this.state.tags];
+		const newTags = tags.slice();
+
+		newTags.splice(currPos, 1);
+		newTags.splice(newPos, 0, tag);
+
+		// re-render
+		this.setState({ tags: newTags });
+	}
+
+
 	render() {
+		const { tags, suggestions } = this.state;
+
 		return(
 			<div className="create_project">
 				<NewProjectNavbar history={this.props.history}/>
@@ -21,7 +58,9 @@ export default class NewProjectPage extends Component {
 						<form>
 							<label>
 								Insert Photo
-								<input type="file" accept="image/*"/>
+								<Row className="justify-content-center">
+									<input type="file" accept="image/*"/>
+								</Row>
 							</label>
 							<label>
 								Name
@@ -30,10 +69,15 @@ export default class NewProjectPage extends Component {
 							<label>
 								Description
 								<textarea className="description"/>
+								</label><label>
+								<ReactTags tags={tags}
+									suggestions={suggestions}
+									handleDelete={this.handleDelete}
+									handleAddition={this.handleAddition}/>
 							</label>
-							<label>
+							<Row className="submit_row justify-content-center">
 								<input type="submit" value="Submit"/>
-							</label>
+							</Row>
 						</form>
 					</div>
 				</Container>
