63ce2420bfe13ae6b6d1358c9af1290d31825f9c
naguilar12 <n.aguilar@uniandes.edu.co>
2018-03-28 18:08:47 -0500

    Saving user information on DB

--- imports/startup/server/service-config.js
+++ imports/startup/server/service-config.js
@@ -1,21 +1,56 @@
 import { Meteor } from "meteor/meteor";
 import { Accounts } from 'meteor/accounts-base'
+import { userInfoPromise } from "../../data"
+import "isomorphic-fetch"
 
-if(Meteor.isServer) {
+
+if (Meteor.isServer) {
 	ServiceConfiguration.configurations.upsert(
 		{ service: 'github' },
 		{
 			$set: {
 				loginStyle: "popup",
-				clientId: Meteor.settings.CLIENT_ID, 
+				clientId: Meteor.settings.CLIENT_ID,
 				secret: Meteor.settings.CLIENT_SECRET
 			}
 		}
 	);
 
 	Accounts.onCreateUser((options, user) => {
-		user.prueba = "asfasff";
-		console.log(user);
-		return user;
+
+		var promisedResult = fetch('https://api.github.com/user?access_token=' + user.services.github.accessToken)
+				.then((res) => {
+					return res.json();
+				})
+				.then((json) => {
+					user.name = json.name;
+					user.image_url = json.avatar_url;
+					user.description = json.bio;
+					return fetch('https://api.github.com/user/repos?access_token=' + user.services.github.accessToken);
+				})
+				.then((res) => {
+					return res.json();
+				})
+				.then((repos) => {
+					user.repos = [];
+
+					var repositories = repos;
+					repositories.map((repo) => {
+						user.repos.push({
+							name: repo.name,
+							description: repo.description,
+							language: repo.language
+						});
+					});
+
+					user.languages = [];
+					user.repos.map((repo) => {
+						if (!user.languages.includes(repo.language) && repo.language)
+							user.languages.push(repo.language);
+					});
+					return user;
+				});
+		
+		return promisedResult.await();
 	});
 }
