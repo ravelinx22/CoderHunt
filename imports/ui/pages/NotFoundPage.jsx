ce39e66b00ce6f63de839c686801f8e7763f70e8
jm-dominguez <jm.dominguez@uniandes.edu.co>
2018-04-01 21:30:00 -0500

    Code Review
    Comentarios code review

--- imports/ui/pages/NotFoundPage.jsx
+++ imports/ui/pages/NotFoundPage.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Hammer from "react-hammerjs";
 import Cards from "../util/Cards.jsx";
-
+//Para usabilidad, me parece una muy buena adición que tengan una página que muestre el error 404 claramente a los usuarios.
 export default class NotFoundPage extends Component {
 	constructor(props) {
 		super(props);
