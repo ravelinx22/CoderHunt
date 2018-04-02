1d3c5c102ccf76f09182a8325a707923f8564eb6
William Ravelo <drummerwilliam@gmail.com>
2018-04-02 08:41:10 -0500

    added photo in looking card

--- imports/ui/util/LookingCard.jsx
+++ imports/ui/util/LookingCard.jsx
@@ -2,13 +2,24 @@ import "../css/LookingCard.css";
 import { Container as Grid, Row, Col, Button } from "reactstrap";
 import React, { Component } from "react";
 import { BounceLoader } from 'react-spinners';
+import { withTracker } from "meteor/react-meteor-data";
+import { Meteor } from "meteor/meteor";
 
-export default class LookingCard extends Component {
+class LookingCard extends Component {
 	constructor(props) {
 		super(props);
 	}
 
 	componentDidMount() {
+	}
+
+	renderImage() {
+		var foundUser = Meteor.users.findOne({_id: Meteor.userId()});
+		if(foundUser && foundUser.image_url) {
+			return foundUser.image_url;
+		}
+
+		return "https://www.thesourcepartnership.com/wp-content/uploads/2017/05/facebook-default-no-profile-pic-300x300.jpg";
 	}
 
 	render() {
@@ -18,9 +29,15 @@ export default class LookingCard extends Component {
 						<div className="spinner">
 							<BounceLoader color={'#193441'} loading={true} className="spinner" />
 						</div> : null }
-						<img className="looking_card_img rounded-circle" src="https://avatars3.githubusercontent.com/u/16025512?s=400&u=e168efefc0d0ff59458573703d521b47b5e82fa0&v=4" alt="looking card" />
+						<img className="looking_card_img rounded-circle" src={this.renderImage()} alt="looking card" />
 						<div className="looking_card_title">{this.props.emptyProjects ? "You don't have proyects yet" : "Searching for results..."}</div>
 					</div>
 		);
 	}
 }
+
+export default withTracker((props) => {
+	return {
+		user: Meteor.user(),
+	}
+})(LookingCard);
