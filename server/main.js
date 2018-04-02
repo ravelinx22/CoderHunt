54e730b386ceaa09e2c5f024cf64c95e9a658e29
jm-dominguez <jm.dominguez@uniandes.edu.co>
2018-04-01 21:31:38 -0500

    Code Review
    Comentarios code review

--- server/main.js
+++ server/main.js
@@ -7,5 +7,5 @@ import "../imports/api/users/Users.js";
 import "../imports/api/chats/Chats.js";
 import "../imports/api/chats/ChatMessages.js";
 import { WebApp } from 'meteor/webapp';
-
+//Me parece muy ben que hallan añadido el meta tag de lang. Esto ayuda a solucionar problemas de accesibilidad.
 WebApp.addHtmlAttributeHook(() => ({ lang: 'en' }));
